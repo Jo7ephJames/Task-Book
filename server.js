589f1077032fcf963fec2496db3c58223170f297
Joe James <38326709+Jo7ephJames@users.noreply.github.com>
2018-09-27 16:57:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 //start sever
-app.set('port', 7444);
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
 	console.log('Server started on port ' +app.get('port'));
 })
